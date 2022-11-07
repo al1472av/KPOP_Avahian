efb18e380ff114d9b7f73581b168f590fe4690ed
Albert <albertavagyan140702@gmail.com>
2022-11-07 13:51:42 +0200

    Lab_3 Done

--- Lab_3/src/app/app.component.ts
+++ Lab_3/src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
      
 @Component({
     selector: 'my-app',
-    template: `<label>Введіть ім’я:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Ласкаво просимо {{name}}!</h1>`
+    template: `<label>Введіть першу цифру:</label>
+                 <input [(ngModel)]="number1" placeholder="name" type="number">
+                 <label>Введіть другу цифру:</label>
+                 <input [(ngModel)]="number2" placeholder="name"type="number">
+                 <h1>+ {{number1 + number2}}</h1>
+                 <h1>* {{number1 * number2}}</h1>
+                 <h1>- {{number1 - number2}}</h1>
+`
 })
 export class AppComponent { 
     name= '';
