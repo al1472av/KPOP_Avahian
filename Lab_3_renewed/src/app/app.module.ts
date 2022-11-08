b889684e48693900bd6cec4b39351bc88c8ef65e
Albert <albertavagyan140702@gmail.com>
2022-11-09 00:06:02 +0200

    Lab_3_Renewd

---
+++ Lab_3_renewed/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImageSearchComponent } from './num-operations/num-operationscomponent';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageSearchComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
